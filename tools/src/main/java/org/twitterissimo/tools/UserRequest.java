48f2135bd1e1d53e7e2456b88a902feef294cbaa
Max Zub <kroistsmeh@gmail.com>
2020-06-09 20:08:53 +0200

    removed friend-request date

--- tools/src/main/java/org/twitterissimo/tools/UserRequest.java
+++ tools/src/main/java/org/twitterissimo/tools/UserRequest.java
@@ -5,10 +5,8 @@ import java.util.Date;
 
 public class UserRequest implements Serializable {
     public ProfileInfo from_whom;
-    public Date request_date;
-    public UserRequest(ProfileInfo from_whom, Date request_date) {
+    public UserRequest(ProfileInfo from_whom) {
         this.from_whom = from_whom;
-        this.request_date = request_date;
     }
 
     public UserRequest() {
